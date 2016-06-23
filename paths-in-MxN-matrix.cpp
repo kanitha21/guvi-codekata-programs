3078943cb94128e19ed6da9490812fc84bc653ed
BalaMurugan21 <balarumugam21@gmail.com>
2016-06-23 15:39:44 +0530

    Update paths-in-MxN-matrix.cpp

--- paths-in-MxN-matrix.cpp
+++ paths-in-MxN-matrix.cpp
@@ -4,10 +4,12 @@ int path(int m, int n)
 {
 if (m == 1 || n == 1)
 		return 1;
+		else if (m<0 && n<0)
+		return 0;
 return path(m-1, n) + path(m, n-1);
 }
 int main()
 {
-	cout << path(4, 4);
+	cout << path(-4, -4);
 	return 0;
 }
